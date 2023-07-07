d53ba78451823a1b94b9f43f512b7d023f2130aa
punni123456 <sadiapunni@gmail.com>
2023-07-07 21:44:14 +0600

    message

---
+++ app/src/main/java/com/labproject/travelassistant/Signup_form.java
@@ -0,0 +1,15 @@
+package com.labproject.travelassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup_form extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_form);
+    }
+}
